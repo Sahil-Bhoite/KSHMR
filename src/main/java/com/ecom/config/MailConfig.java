d326d2794b019229b5d310d713a959325f9abd18
Sahil Bhoite <sahil.bhoite@mitaoe.ac.in>
2025-01-14 12:14:15 +0530

    Version 2

---
+++ src/main/java/com/ecom/config/MailConfig.java
@@ -0,0 +1,13 @@
+package com.ecom.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailConfig {
+
+    // Add mail configuration beans here
+
+}
